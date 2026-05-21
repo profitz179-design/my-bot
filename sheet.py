6f14726872a28fde57b1ed671b27ac291f283efc
profitz179-design <profitz179@gmail.com>
2026-05-21 16:28:12 +0100

    Update sheet.py

--- sheet.py
+++ sheet.py
@@ -1,11 +1,16 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import os
+import json
 
 def get_sheet():
     scopes = ["https://spreadsheets.google.com/feeds",
               "https://www.googleapis.com/auth/drive"]
-    creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+    
+    creds_json = os.getenv("GOOGLE_CREDENTIALS")
+    creds_dict = json.loads(creds_json)
+    creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
+    
     client = gspread.authorize(creds)
     return client.open_by_key(os.getenv("GOOGLE_SHEET_ID")).sheet1
 
